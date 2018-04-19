358d7e686c65af2b9e053f3fe127cbff9e169754
Emir Bucalovic <emir.bucalovic@tactel.se>
2018-04-19 12:36:47 +0200

    Added dependency JUnitParams. Added more tests to FizzTests.

--- src/main/java/FizzStringReplacer.java
+++ src/main/java/FizzStringReplacer.java
@@ -1,8 +1,12 @@
 
+@SuppressWarnings("javadoc")
 public class FizzStringReplacer implements StringReplacer{
 
 	@Override
 	public String fizzBuzzer(int i) {
+		if(i%3 == 0) {
+			return "Fizz";
+		}
 		return String.valueOf(i);
 	}
 
--- src/test/java/FizzTests.java
+++ src/test/java/FizzTests.java
@@ -1,13 +1,20 @@
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+
+@SuppressWarnings("javadoc")
+@RunWith(JUnitParamsRunner.class)
 public class FizzTests {
 
 	@Test
-	public void returnsStringOneOnIntInputOne() {
+	@Parameters({"1,1","2,2","3,Fizz","4,4","5,5","6,Fizz","7,7","8,8","9,Fizz","100,100","300,Fizz"})
+	public void returnsFizzStringOnIntegerDividableWithThree(int inputInt, String expected) {
 		StringReplacer fizzer = new FizzStringReplacer();
-		assertEquals("1", fizzer .fizzBuzzer(1));
+		assertEquals(expected, fizzer .fizzBuzzer(inputInt));
 	}
-
 }
