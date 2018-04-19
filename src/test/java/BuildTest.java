7f7964b74a444d5d1c63bb5f82fcb5b40eb28312
Emir Bucalovic <emir.bucalovic@tactel.se>
2018-04-19 12:10:53 +0200

    Updated gradle configuration and added simple build test.

---
+++ src/test/java/BuildTest.java
@@ -0,0 +1,15 @@
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class BuildTest {
+
+	@Test
+	public void test() {
+		boolean condition = true;
+		assertTrue(condition );
+	}
+
+}
